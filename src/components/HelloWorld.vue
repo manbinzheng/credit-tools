fce1b855102e3eb7f7ba20470fe6addddc85d79c
manbinzheng <manbinzheng@gmail.com>
2023-09-17 14:58:22 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,88 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1 v-if="maxCards.length">
+      今天使用<a>{{ maxCards.join(",") }}</a
+      >信用卡免费用款时间最长！免费使用天数为：<a>{{ freeDays }}</a>
+    </h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  mounted() {
+    this.today = new Date();
+    this.thisMonth = this.today.getMonth();
+    // 计算每个卡的免费试用时间
+    const interestFreeTimes = this.cards.map((card) => {
+      // 获取免息时长（毫秒）
+      const freeTimes = this.getFreeTimes(card.billDay, card.repayDay);
+      // 获取下一个还款日
+      const nextBillDate = this.getNextBillDate(card.billDay);
+      // 下一个还款日距离当前时间 + 免息时间
+      return nextBillDate - this.today + freeTimes;
+    });
+
+    // 免费使用时间最多值
+    const maxInterestFreeTimes = Math.max(...interestFreeTimes);
+
+    // 过滤出免费使用时间最多的卡
+    this.maxCards = this.cards.filter((_, index) => {
+      return interestFreeTimes[index] === maxInterestFreeTimes;
+    }).map(card => card.name);
+    // 输出最佳信用卡们
+    console.log("最佳信用卡:");
+    this.maxCards.forEach((card) => console.log(card.name));
+    this.freeDays = Math.ceil(maxInterestFreeTimes / (1000 * 60 * 60 * 24));
+    console.log(
+      "免息天数:",
+      Math.ceil(maxInterestFreeTimes / (1000 * 60 * 60 * 24))
+    );
+  },
+  data: function () {
+    return {
+      today: 0,
+      thisMonth: 0,
+      cards: [
+        { name: "中国银行", billDay: 7, repayDay: 27 },
+        { name: "中信银行", billDay: 16, repayDay: 5 },
+        { name: "民生银行", billDay: 11, repayDay: 1 },
+        { name: "广发银行", billDay: 9, repayDay: 28 },
+        { name: "交通银行", billDay: 27, repayDay: 21 },
+        { name: "招商银行", billDay: 9, repayDay: 27 },
+        { name: "平安银行", billDay: 18, repayDay: 6 },
+      ],
+      maxCards: [],
+      freeDays: 0,
+    };
+  },
+  methods: {
+    /**
+     * 获取免息时长（毫秒）
+     */
+    getFreeTimes(billDay, repayDay) {
+      const billDate = new Date(this.today.getFullYear(), 0, billDay);
+      const repayDate = new Date(
+        this.today.getFullYear(),
+        repayDay > billDay ? 0 : 1,
+        repayDay
+      );
+      return repayDate - billDate;
+    },
+
+    /**
+     * 获取下一个账单日期
+     * @param {*} billDay 账单日
+     */
+    getNextBillDate(billDay) {
+      const date = new Date(this.today.getFullYear(), this.thisMonth, billDay);
+      if (billDay <= this.today.getDate()) {
+        date.setMonth(date.getMonth() + 1);
+      }
+      return date;
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -3,6 +3,65 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+
+// 信用卡信息
+const cards = [
+  {name: '中国银行', billDay: 7, repayDay: 27},
+  {name: '中信银行', billDay: 16, repayDay: 5}, 
+  {name: '民生银行', billDay: 11, repayDay: 1},
+  {name: '广发银行', billDay: 9, repayDay: 28},
+  {name: '交通银行', billDay: 27, repayDay: 21},
+  {name: '招商银行', billDay: 9, repayDay: 27},
+  {name: '平安银行', billDay: 18, repayDay: 6},
+];
+
+// 当前日期  
+const today = new Date();
+const thisMonth = today.getMonth();
+
+// 计算每个卡的免费试用时间
+const interestFreeTimes = cards.map(card => {
+  // 获取免息时长（毫秒）
+  const freeTimes = getFreeTimes(card.billDay, card.repayDay);
+  // 获取下一个还款日
+  const nextBillDate = getNextBillDate(card.billDay);
+  // 下一个还款日距离当前时间 + 免息时间
+  return nextBillDate - today + freeTimes;
+});
+
+// 免费使用时间最多值
+const maxInterestFreeTimes = Math.max(...interestFreeTimes);
+
+// 过滤出免费使用时间最多的卡
+const maxCards = cards.filter((_,index) => {
+  return interestFreeTimes[index] === maxInterestFreeTimes;
+});
+// 输出最佳信用卡们
+console.log('最佳信用卡:');
+maxCards.forEach(card => console.log(card.name));
+
+console.log('免息天数:', Math.ceil(maxInterestFreeTimes / (1000 * 60 * 60 * 24)));
+
+
+/**
+ * 获取免息时长（毫秒）
+ */
+function getFreeTimes(billDay, repayDay) {
+  const billDate = new Date(today.getFullYear(), 0, billDay)
+  const repayDate = new Date(today.getFullYear(), repayDay > billDay ? 0 : 1, repayDay)
+  return repayDate - billDate;
+}
+
+function getNextBillDate(billDay) {
+  const date = new Date(today.getFullYear(), thisMonth, billDay);
+  if(billDay <= today.getDate()) {
+    date.setMonth(date.getMonth() + 1);
+  }
+  return date;
+  // const nextMonth = billDay <= today.getDate() ? thisMonth + 1 : thisMonth;
+  // return new Date(today.getFullYear(), nextMonth, billDay);
+}
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
